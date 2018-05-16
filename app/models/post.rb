7b7141b958c88f6c9e6597a02c05e1a44d65bb72
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-16 04:20:33 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 100}
-  category :category, inclusion: { in: %w(Fiction Non-Fiction)
+  category :category, inclusion: { in: %w(Fiction Non-Fiction)}
 end
